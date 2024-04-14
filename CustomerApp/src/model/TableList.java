608867cd58635a0a8435def03a361c1d63b30de1
samosusa <samosusa123@gmail.com>
2024-04-14 09:31:55 +0200

    viewmodel

--- CustomerApp/src/model/TableList.java
+++ CustomerApp/src/model/TableList.java
@@ -1,22 +1,36 @@
 package model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 
-public class TableList implements TableManagement {
+import utility.observer.javaobserver.UnnamedPropertyChangeSubject;
+
+public class TableList implements TableManagement, UnnamedPropertyChangeSubject {
     private ArrayList<Table> tables;
+    private PropertyChangeSupport propertyChangeSupport;
 
     public TableList() {
         tables = new ArrayList<>();
+        propertyChangeSupport = new PropertyChangeSupport(this);
     }
 
-    // Method to create a new table
+    @Override
+    public void addListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    @Override
+    public void removeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
     @Override
     public void createTable(int tableNumber, int capacity) {
         Table newTable = new Table(tableNumber, capacity);
         tables.add(newTable);
     }
 
-    // Method to delete a table
     @Override
     public void deleteTable(int tableNumber) {
         for (int i = 0; i < tables.size(); i++) {
@@ -27,7 +41,6 @@ public class TableList implements TableManagement {
         }
     }
 
-    // Method to update the status of a table
     @Override
     public void updateTable(int tableNumber, boolean isOccupied) {
         for (Table table : tables) {
@@ -42,15 +55,14 @@ public class TableList implements TableManagement {
         }
     }
 
-    // Method to get the number of tables
     public int getNumberOfTables() {
         return tables.size();
     }
 
-    // Method to get all tables
     public ArrayList<Table> getAllTables() {
         return tables;
     }
+
     public ArrayList<Table> getVacantTables() {
         ArrayList<Table> vacantTables = new ArrayList<>();
         for (Table table : tables) {
@@ -61,5 +73,3 @@ public class TableList implements TableManagement {
         return vacantTables;
     }
 }
-
-
--- CustomerApp/src/viewmodel/TablesViewModel.java
+++ CustomerApp/src/viewmodel/TablesViewModel.java
@@ -10,8 +10,60 @@ import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.ModelManager;
-
-
-
 public class TablesViewModel {
+    private StringProperty errorProperty;
+    private IntegerProperty tableNumberProperty;
+    private IntegerProperty capacityProperty;
+    private BooleanProperty occupiedProperty;
+
+    private TableList tableList;
+
+    public TablesViewModel(TableList tableList) {
+        this.tableList = tableList;
+
+        errorProperty = new SimpleStringProperty();
+        tableNumberProperty = new SimpleIntegerProperty();
+        capacityProperty = new SimpleIntegerProperty();
+        occupiedProperty = new SimpleBooleanProperty();
+    }
+
+    public void deleteTable(int tableNumber) {
+        try {
+            tableList.deleteTable(tableNumber);
+        } catch (IllegalArgumentException e) {
+            errorProperty.set(e.getMessage());
+        }
+    }
+
+    public void createTable() {
+        try {
+            tableList.createTable(tableNumberProperty.get(), capacityProperty.get());
+        } catch (IllegalArgumentException e) {
+            errorProperty.set(e.getMessage());
+        }
+    }
+
+    public void updateTable() {
+        try {
+            tableList.updateTable(tableNumberProperty.get(), occupiedProperty.get());
+        } catch (IllegalArgumentException e) {
+            errorProperty.set(e.getMessage());
+        }
+    }
+
+    public StringProperty getErrorProperty() {
+        return errorProperty;
+    }
+
+    public IntegerProperty getTableNumberProperty() {
+        return tableNumberProperty;
+    }
+
+    public IntegerProperty getCapacityProperty() {
+        return capacityProperty;
+    }
+
+    public BooleanProperty getOccupiedProperty() {
+        return occupiedProperty;
+    }
 }
